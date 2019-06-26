9d9435d2326b54186a4bd72efca79f87de49a78d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-06-26 12:19:04 +0200

    Add run_test.py

---
+++ recipe/run_test.py
@@ -0,0 +1,8 @@
+import ixmp
+import message_ix.testing
+
+print(ixmp.__version__)
+print(message_ix.__version__)
+
+mp = ixmp.Platform(dbtype='HSQLDB')
+message_ix.testing.make_westeros(mp, emissions=True, solve=True)
