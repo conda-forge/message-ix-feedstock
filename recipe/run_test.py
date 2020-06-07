e930024d13be584190fc0623631d4b2c734cd8cd
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-06-07 16:44:10 +0200

    Remove unused recipe/run_test.py

--- recipe/run_test.py
+++
@@ -1,8 +0,0 @@
-import ixmp
-import message_ix.testing
-
-print(ixmp.__version__)
-print(message_ix.__version__)
-
-mp = ixmp.Platform(dbtype='HSQLDB')
-message_ix.testing.make_westeros(mp, emissions=True, solve=False)
